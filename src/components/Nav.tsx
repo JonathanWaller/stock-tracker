43ba6959d6417c071e6f7905d30da8380dab25b7
JonathanWaller <waller.j27@gmail.com>
2023-04-15 12:30:02 -0400

    adds favicong, home nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,25 +1,46 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 import NavSearch from './Search/NavSearch';
+import Image from 'next/image';
+
+import Logo from '../../public/st-logo.png';
+
 
 const NavContainer = styled.div`
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
     align-items: center;
-    gap: 25px;
     background: black;
     position: fixed;
     width: 100%;
     top: 0;
     left: 0;
-    padding: 10px;
+    padding: 10px 30px;
+`
+
+const NavInnerContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 25px;
 `
 
 const Nav = () => {
     return(
         <NavContainer>
-            <Link href={'/portfolio'}>Watch List</Link>
-            <NavSearch />
+            <Link href={'/'}>
+                <Image
+                    src={Logo}
+                    alt="logo"
+                    width={35}
+                    height={35}
+                />
+            </Link>
+            <NavInnerContainer>
+                <Link href={'/portfolio'}>Watch List</Link>
+                <NavSearch />
+            </NavInnerContainer>
+            
         </NavContainer>
     )
 }
